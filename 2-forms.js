577a7b9f32b9ac874acdd89070da64ba58dd8c8a
tommaso.panciroli <tommypanci@gmail.com>
2024-10-16 11:55:13 +0200

    adds forms's files with some examples

---
+++ 2-forms.js
@@ -0,0 +1,5 @@
+const kittensButton = document.getElementById('kittens')
+kittensButton.addEventListener('click', function () {
+  location.assign('https://placecats.com/300/200')
+  // apre la pagina https://placecats.com/300/200
+})
